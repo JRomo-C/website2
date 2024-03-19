09573dbdff6cd928b6718e3fa03a89854e02ee27
Joaquin Romo <142829224+JRomo-C@users.noreply.github.com>
2024-03-19 17:14:48 +0000

    Tue, Mar 19, 2024, 10:14 AM -07:00

--- breakout.js
+++ breakout.js
@@ -1,6 +1,8 @@
 open = document.getElementById('rules-btn')
 close = document.getElementById('close-btn')
 rules = document.getElementById('rules')
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
 
 // Rules open and close event handlers
 open.addEventListener('click',() => {

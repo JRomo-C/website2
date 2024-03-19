38f42c97ce201cac9d9528e47c35cdafde75690a
Joaquin Romo <142829224+JRomo-C@users.noreply.github.com>
2024-03-19 17:18:16 +0000

    Tue, Mar 19, 2024, 10:18 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -4,6 +4,24 @@ rules = document.getElementById('rules')
 canvas = document.getElementById('canvas')
 ctx = canvas.getContext('2d')
 
+// Create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4;
+    dy: -4;
+}
+
+// Draw ball on canvas
+function drawBall() {
+    
+}
+
+
+
+
 // Rules open and close event handlers
 open.addEventListener('click',() => {
     rules.classList.add('show')

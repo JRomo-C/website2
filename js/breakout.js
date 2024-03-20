a6737868c925cf72af1e10017f996f873bbd67df
Joaquin Romo <142829224+JRomo-C@users.noreply.github.com>
2024-03-20 17:12:16 +0000

    Wed, Mar 20, 2024, 10:12 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -45,8 +45,10 @@ bricks = []
 for (let i = 0; i < brickRowCount; i++) {
     bricks[i] = []
     for (let j = 0; j < brickColumnCount; j++) {
-        let x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY
     }
+
 }
 
 // Draw ball on canvas

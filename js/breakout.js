3d6d3b56cb9e89156b4383f48348fdb83905d076
Joaquin Romo <142829224+JRomo-C@users.noreply.github.com>
2024-03-20 16:29:36 +0000

    Wed, Mar 20, 2024, 9:29 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -14,11 +14,26 @@ ball = {
     dy: -4,
 }
 
+// Create paddle properties
+paddle = {
+    x: canvas.width /2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+
+}
+
+
+
+
 // Draw ball on canvas
 function drawBall() {
     ctx.beginPath()
     ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2, true)
-    ctx.stroke()
+    ctx.fillStyle = 'black'
+    ctx.fill()
 }
 
 drawBall();

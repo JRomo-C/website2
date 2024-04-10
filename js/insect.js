184d52edd503ae48abfd27cd1aa71349df7dc82a
Joaquin Romo <142829224+JRomo-C@users.noreply.github.com>
2024-04-10 17:26:30 +0000

    Wed, Apr 10, 2024, 10:26 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -46,7 +46,7 @@ function increaseTime() {
 function createInsect() {
     const insect = document.createElement('div')
     insect.classList.add('insect')
-    const { x, y } = getRandomLocation()
+    const {x, y} = getRandomLocation()
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
     insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insect.alt}" style= "transform: rotate(${Math.random() * 360}deg" />`

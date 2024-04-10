f21754d59921e8e26a3dd554a78de58c9f98d63f
Joaquin Romo <142829224+JRomo-C@users.noreply.github.com>
2024-04-10 16:59:25 +0000

    Wed, Apr 10, 2024, 9:59 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -79,6 +79,6 @@ function increaseScore() {
 function getRandomLocation() {
     const width = window.innerWidth
     const height = window.innerHeight
-    const x = Math.random() * (width - 200) + 100
-    const y = Math.random() * (height - 200) + 100
+    const x = math.random() * (width - 200) + 100
+    const y = math.random() * (height - 200) + 100
 }

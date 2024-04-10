09aa584903ffc36ad9ca7b1090f56ae23dc871c5
Joaquin Romo <142829224+JRomo-C@users.noreply.github.com>
2024-04-10 16:26:06 +0000

    Wed, Apr 10, 2024, 9:26 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -32,11 +32,22 @@ function createInsect() {
     const {x, y} = getRandomLocation()
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
-    insect.innerHTML = `<img src=${selected_insect.src} alt=${selected_insect.alt} style=${transform: rotate(${math.random() * 360}deg)}/>`
-
+    insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insect.alt}" style= "transform: rotate(${math.random() * 360}deg")/>`
+    insect.addEventListener('click', catchInsect)
     game_container.appendChild(insect)
 }
 
+
+function catchInsect() {
+    increaseScore()
+}
+
+function increaseScore() {
+    score++
+    if (score > 19) {
+        message.classList.add('visible')
+    }
+}
 
 
 function getRandomLocation() {

b5b610d49526c1823d9cc6e8f0f41f8ed6fc1092
Joaquin Romo <142829224+JRomo-C@users.noreply.github.com>
2024-04-22 16:47:12 +0000

    Mon, Apr 22, 2024, 9:47 AM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -2,3 +2,8 @@ const wordEL = document.getElementById('word')
 wronglettersEL = document.getElementById('wrong-letters')
 const playAgainBtn = document.getElementById('play-again')
 const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
+const figureParts = document.querySelectorAll('.figure-part')
+
+const word = ['application', 'programming', 'interface', 'wizard']

96208e50e9c94281b5754eb27b0008491ca34673
Joaquin Romo <142829224+JRomo-C@users.noreply.github.com>
2024-04-22 17:16:37 +0000

    Mon, Apr 22, 2024, 10:16 AM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -36,7 +36,9 @@ function displayWord() {
 
 //Keydown letter press
 window.addEventListener('keydown', e => {
-    console.log(e.keyCode)
+
+    if (e.keyCode >= 65 && e.keyCode <=90) {
+    }
 })
 
 displayWord()

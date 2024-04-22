cea1ef4441c1f868dae58334ca3ea6acab1ca3c1
Joaquin Romo <142829224+JRomo-C@users.noreply.github.com>
2024-04-22 16:44:00 +0000

    Mon, Apr 22, 2024, 9:44 AM -07:00

---
+++ js/hangman.js
@@ -0,0 +1,4 @@
+const wordEL = document.getElementById('word')
+wronglettersEL = document.getElementById('wrong-letters')
+const playAgainBtn = document.getElementById('play-again')
+const popup = document.getElementById('popup-container')
